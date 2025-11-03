1037873698a733236da845e441d02d3ad83b3e46
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-11-03 18:04:50 -0500

    go back to old viz design, clean up

--- frontend/src/util/math.ts
+++ frontend/src/util/math.ts
@@ -3,10 +3,6 @@ import { clamp } from "lodash";
 /** 2 pi */
 export const tau = 2 * Math.PI;
 
-/** trig in degrees */
-export const sin = (degrees: number) => Math.sin(tau * (degrees / 360));
-export const cos = (degrees: number) => Math.cos(tau * (degrees / 360));
-
 /** linear interpolate */
 export const lerp = (
   value: number,
@@ -18,16 +14,3 @@ export const lerp = (
   targetMin +
   clamp((value - sourceMin) / (sourceMax - sourceMin || 1), 0, 1) *
     (targetMax - targetMin);
-
-/** 2d distance */
-export const dist = (ax = 0, ay = 0, bx = 0, by = 0) =>
-  Math.hypot(bx - ax, by - ay);
-
-/** polar to cartesian */
-export const polar = (radius: number, angle: number) => ({
-  x: radius * cos(angle),
-  y: radius * sin(angle),
-});
-
-/** deg to rad */
-export const rad = (degrees: number) => (degrees / 360) * tau;
