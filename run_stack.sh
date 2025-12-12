e88def8a92377769d4b1fdc0d1d33494e185906d
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-12-12 16:08:43 -0700

    run_stack.sh acquires database dump from bucket if local copy is old or not present

--- run_stack.sh
+++ run_stack.sh
@@ -11,6 +11,9 @@
 
 set -euo pipefail
 
+# if 1, skips interactive prompts
+NONINTERACTIVE=${NONINTERACTIVE:-"0"}
+
 # ================================================================
 # == set up .env file
 # ================================================================
@@ -44,6 +47,51 @@ replace_env_var_with_secret DJANGO_SECRET_KEY 32
 set -a
 source .env
 set +a
+
+# ================================================================
+# == obtain database dump to initialize db
+# ================================================================
+
+# Converts bytes value to human-readable string [$1: bytes value]
+# from https://unix.stackexchange.com/a/259254
+bytesToHumanReadable() {
+    local i=${1:-0} d="" s=0 S=("Bytes" "KiB" "MiB" "GiB" "TiB" "PiB" "EiB")
+    while ((i > 1024 && s < ${#S[@]}-1)); do
+        printf -v d ".%02d" $((i % 1024 * 100 / 1024))
+        i=$((i / 1024))
+        s=$((s + 1))
+    done
+    echo "$i$d ${S[$s]}"
+}
+
+LATEST_DUMP_URL="https://storage.googleapis.com/cu-dbmi-meta2onto/meta2onto_latest.dump"
+
+LAST_DUMP_FILENAME=$( basename "${LATEST_DUMP_URL}" )
+TARGET_DB_DUMP="./db-exports/${LAST_DUMP_FILENAME}"
+
+REMOTE_FILE_SIZE=$( curl -sI "${LATEST_DUMP_URL}" | grep -i '^Content-Length:' | awk '{print $2}' | tr -d '\r\n' )
+LOCAL_FILE_SIZE=$(wc -c < "$TARGET_DB_DUMP" 2>/dev/null || echo 0)
+
+# if the local dump either doesn't exist or doesn't match the remote's file size, download it
+if [ ! -f "${TARGET_DB_DUMP}" ] || [ "${REMOTE_FILE_SIZE}" -ne "${LOCAL_FILE_SIZE}" ]; then
+	# if we're in interactive mode, confirm before downloading
+	if [ "${NONINTERACTIVE}" -eq 0 ]; then
+		read -p "* A new database dump is available; it will require $( bytesToHumanReadable ${REMOTE_FILE_SIZE} ) of space. Download it now? (y/n) " yn
+		case $yn in
+			[Yy]* )
+				echo "* Downloading latest database dump from ${LATEST_DUMP_URL}..."
+				mkdir -p ./db-exports/
+				time curl -o "./db-exports/${LAST_DUMP_FILENAME}" -L "${LATEST_DUMP_URL}"
+				;;
+			* )
+				echo "* Skipping database dump download"
+		esac
+	else
+		echo "* Non-interactive mode: downloading latest database dump from ${LATEST_DUMP_URL}..."
+		mkdir -p ./db-exports/
+		time curl -o "./db-exports/${LAST_DUMP_FILENAME}" -L "${LATEST_DUMP_URL}"
+	fi
+fi
 
 # ================================================================
 # == determine environment (i.e., which compose files to use)
