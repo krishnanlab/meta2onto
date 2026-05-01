adcc58229967e0c0617f16006fce0ff6312d737f
phicks22 <we.can.phicks.it@gmail.com>
2026-05-01 11:44:15 -0600

    added description of pyrefinebio for token generation

--- scripts/refinebio_data_cart.py
+++ scripts/refinebio_data_cart.py
@@ -1,3 +1,17 @@
+"""
+Previously, I tried generating a token using the pyrefinebio package, but importing the package
+raises the following:
+
+    ModuleNotFoundError .../pyrefinebio/api_interface.py", line 5, in <module>
+        from pyrate_limiter import Duration, Limiter, RequestRate
+    ModuleNotFoundError: No module named 'pyrate_limiter'
+
+I saw this recent issue: https://github.com/AlexsLemonade/refinebio-py/issues/90 explaining that you
+need to install pyrate_limiter<3 to fix this. After installing that, the pyrefinebio package imports
+properly.
+
+"""
+
 import requests
 
 
@@ -38,6 +52,10 @@ def post_dataset(data: dict[str, list[str]], token: str):
 
 def main():
     data = {"SRP081083": ["SRR4011226", "SRR4011227", "SRR4011228", "SRR4011229"]}
+
+    # Can use pyrefinebio or the API. Prefer API
+    # import pyrefinebio
+    # token = pyrefinebio.create_token(agree_to_terms=True, save_token=True)
 
     token = post_token()
     result = post_dataset(data, token)
