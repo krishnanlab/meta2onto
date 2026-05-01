99a7fa2825bc9b659aad4e7716b24847f3890e08
phicks22 <we.can.phicks.it@gmail.com>
2026-05-01 13:40:14 -0600

    added extra docs to refine.bio data cart example

--- scripts/refinebio_data_cart.py
+++ scripts/refinebio_data_cart.py
@@ -10,17 +10,30 @@ I saw this recent issue: https://github.com/AlexsLemonade/refinebio-py/issues/90
 need to install pyrate_limiter<3 to fix this. After installing that, the pyrefinebio package imports
 properly.
 
+Questions:
+    - How many tokens are required?
+    - Is there a limit for the number of datasets a token can create or maintain at a time?
+
+
+Author: Parker Hicks
+Date: 2026-05-01
+
 """
 
 import requests
 
+API_TOKEN_URL = "https://api.refine.bio/v1/token/"
+API_DATASET_URL = "https://api.refine.bio/v1/dataset/"
+
+DATA_CART_URL = "https://www.refine.bio/dataset/"
+
 
 def post_token():
     """Recieve an API token from refine.bio."""
-    response = requests.post("https://api.refine.bio/v1/token/")
+    response = requests.post(API_TOKEN_URL)
     token_id = response.json()["id"]
     response = requests.put(
-        "https://api.refine.bio/v1/token/" + token_id + "/",
+        API_TOKEN_URL + token_id + "/",
         json={"is_activated": True},
     )
 
@@ -38,7 +51,7 @@ def post_dataset(data: dict[str, list[str]], token: str):
 
     """
     response = requests.post(
-        "https://api.refine.bio/v1/dataset/",
+        API_DATASET_URL,
         json={
             "data": data,
             "email_ccdl_ok": False,
@@ -53,7 +66,15 @@ def post_dataset(data: dict[str, list[str]], token: str):
 def main():
     data = {"SRP081083": ["SRR4011226", "SRR4011227", "SRR4011228", "SRR4011229"]}
 
-    # Can use pyrefinebio or the API. Prefer API
+    # If you add a study ID that is not in refine.bio, it throws an error:
+    # requests.exceptions.HTTPError: 400 Client Error: Bad Request for url: https://api.refine.bio/v1/dataset/
+    # Try with data below:
+    # data = {
+    #    "SRP081083": ["SRR4011226", "SRR4011227", "SRR4011228", "SRR4011229"],
+    #    "test": ["not_in_dataset1", "not_in_dataset2"],
+    # }
+
+    # Can use pyrefinebio or the API to create a token. Prefer API
     # import pyrefinebio
     # token = pyrefinebio.create_token(agree_to_terms=True, save_token=True)
 
@@ -62,6 +83,8 @@ def main():
 
     print("token:", token)
     print("dataset:", result)
+    print("")
+    print(f"populated data cart available at {DATA_CART_URL}{result['id']}")
 
 
 if __name__ == "__main__":
