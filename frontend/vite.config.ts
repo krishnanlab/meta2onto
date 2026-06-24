609adf56a18c67cd8263f891a62d588f15011cbe
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-06-23 21:17:58 -0400

    remove svgr (not used)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,8 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    svgr({
-      svgrOptions: {
-        /** https://github.com/gregberge/svgr/discussions/770 */
-        expandProps: "start",
-        svgProps: {
-          className: `{props.className ? props.className + " lucide" : "lucide"}`,
-          "aria-hidden": "true",
-        },
-      },
-    }),
-  ],
+  plugins: [tailwindcss(), react()],
   resolve: { alias: { "@": "/src" } },
 });
