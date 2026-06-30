e3000476c87ccdbc81eeb5c4dd38f2d8d6ffc238
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-06-30 10:38:25 -0400

    use tsconfigpaths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,5 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  resolve: { alias: { "@": "/src" } },
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
