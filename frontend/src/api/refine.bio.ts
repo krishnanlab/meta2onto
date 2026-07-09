2153fe9b41238dba125b0410c6eeaccdb4d734de
phicks22 <we.can.phicks.it@gmail.com>
2026-07-09 08:33:50 -0600

    fix: fixes refine.bio export POST request
    Previously, the payload specified start: true, however this begins the data normalization and downloading procedure for a user. This requres a token and the user must agree to refine.bio's licensing agreement. I changed the start value to false and removed the quantile_normalized, quant_sf_only, and svd_algorithm. This will still create a refine.bio dataset and users will go there to select the download parameters on the the refine.bio download page. We can probably get rid of the email input too since that is also related to starting the processing and downloading procedure.

--- frontend/src/api/refine.bio.ts
+++ frontend/src/api/refine.bio.ts
@@ -19,15 +19,10 @@ export const makeDataset = async (ids: string[], email = "") => {
     headers: { "Content-Type": "application/json" },
     body: {
       data: Object.fromEntries(ids.map((id) => [id, ["ALL"]])),
-      aggregate_by: "ALL",
-      scale_by: "NONE",
       email_address: email,
       email_ccdl_ok: !!email,
       notify_me: !!email,
-      start: true,
-      quantile_normalize: true,
-      quant_sf_only: true,
-      svd_algorithm: "NONE",
+      start: false,
     },
   } as const;
   const data = await request(url, dataset, options);
